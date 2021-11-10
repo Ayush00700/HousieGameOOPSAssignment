f04c371059e1213a2de929b3e15a09e596f6ea62
Ayush Agarwal <65657463+Ayush00700@users.noreply.github.com>
2021-11-10 14:49:24 +0530

    Cleaning of Code

--- Ticket.java
+++ Ticket.java
@@ -18,45 +18,6 @@ public class Ticket {
     public String getPlayerName(){
         return playerName;
     }
-    public int[][] generateTicket2(){
-        int arr[] = new int[9];
-        int sum = 0;
-        while(sum!=6){
-            sum=0;
-            for(int i=0;i<9;i++){
-                arr[i] = (int)(3*Math.random());
-                sum+=arr[i];
-            }
-        }
-        for(int col=0;col<9;col++){
-            
-            int no1; int no2; int no3;
-            do{
-                no1= (int)(11*Math.random());
-            }while(no1==0);
-            do{
-                no2= (int)(11*Math.random());
-            }while(no2==0||no2==no1);
-        
-            do{
-                no3= (int)(11*Math.random());
-            }while(no3==0||no3==no1||no3==no2);
-            
-            int[] arr2= {no1,no2,no3}; 
-            Arrays.sort(arr2);
-            // System.out.println(Arrays.toString(arr2));
-
-                card[0][col] = arr2[0]+(10*col);
-                card[1][col] = arr[col]<=1? 0:arr2[1]+(10*col);
-                card[2][col] = arr[col]==0? 0:arr2[2]+(10*col);
-        }
-        
-        // for(int i:arr){
-        //     System.out.print(i);
-        // }
-        // System.out.println();
-        return card;
-    }
     public int[][] generateTicket(){
         List<Integer> arr = new ArrayList<>();
         int[][] cardPositions = new int[3][9];
@@ -102,9 +63,6 @@ public class Ticket {
             }
         }
 
-        // for(int[] i:cardPositions){
-        //     System.out.println(Arrays.toString(i));
-        // }
         card = cardPositions;
         return card;
     }
